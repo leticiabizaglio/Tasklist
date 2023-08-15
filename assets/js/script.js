cb52320061bc00e99c66b6bedd76544c370687b4
leticiabizaglio <leticia.bizaglio@aluno.senai.br>
2023-08-15 16:52:41 -0300

    feat: start of JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,5 @@
 
+let armazenar = [];
 
 function add() {
     let init = document.getElementById("tarefa").value;
@@ -8,7 +9,9 @@ function add() {
         alert("Digite algo válido!");
         return;
     }
+    init.push(information);
 }
+console.log(armazenar);
 class Task {
     constructor(id, title, status) {
             this.id = id,
@@ -16,12 +19,37 @@ class Task {
             this.status = status
     }
 }
+
+// OBS: TIRAR ESSA PARTE DO HTML E DEIXAR SOMENTE NO JS
+
+
+// function showForm() {
+//     document.getElementById("list").classList.remove("hidden");
+//     let showContent = "";
+
+//     posts.forEach((post, index) => {
+//         showContent += `
+//         <div id="segundaria">
+
+//         <div id="form2">
+//             <div id="info">
+//                 <span id="text">Isso é só um teste!</span>
+//             </div>
+//             <div id="botoes">
+//                 <button type="button" id="ok"><i class="fa-solid fa-check"></i></button>
+//                 <button type="button" id="edit"><i class="fa-regular fa-pen-to-square"></i></button>
+//                 <button type="button" id="delete"><i class="fa-solid fa-trash"></i></button>
+//             </div>
+//         </div>
+//     </div>
+//         `
+//     });
+// }
+
 class Tasklist {
     constructor() {
         this.tasks = [];
     }
-
-    addTask(task).push(task);
 }
 
 function randonId() {
